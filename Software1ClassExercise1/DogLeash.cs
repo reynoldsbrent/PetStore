6f2b3e741a50d19898dc0ddc6ae8704aea67ac7a
Brent Reynolds <brentsreynolds5@gmail.com>
2024-04-03 14:37:59 -0400

    Added required functionality for Exercise 1

---
+++ Software1ClassExercise1/DogLeash.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Software1ClassExercise1
+{
+    public class DogLeash:Product
+    {
+        public int LengthInches { get; set; }
+        public string Material { get; set; }
+
+    }
+}
--- Software1ClassExercise1/Program.cs
+++ Software1ClassExercise1/Program.cs
@@ -1,10 +1,50 @@
-﻿namespace Software1ClassExercise1
+﻿using System;
+using System.Text.Json;
+
+namespace Software1ClassExercise1
 {
-    internal class Program
+    public class Program
     {
+        public static string userInput;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Press 1 to add a product");
+            Console.WriteLine("Type 'exit' to quit");
+            userInput = Console.ReadLine();
+
+            while(userInput.ToLower() != "exit")
+            {
+                if (userInput == "1")
+                {
+                    DogLeash leash = new DogLeash();
+                    Console.WriteLine("Adding the leash to product inventory.");
+
+                    Console.WriteLine("Enter a description of the leash: ");
+                    leash.Description = Console.ReadLine();
+
+                    Console.Write("What is the name of the leash: ");
+                    leash.Name = Console.ReadLine();
+
+                    Console.Write("Enter the length of the leash: ");
+                    leash.LengthInches = int.Parse(Console.ReadLine());
+
+                    Console.Write("Enter the material of the leash: ");
+                    leash.Material = Console.ReadLine();
+
+                    Console.WriteLine("Enter the price of the leash in dollars: ");
+                    leash.Price = decimal.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Enter the amount of leashes in stock: ");
+                    leash.Quantity = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine(JsonSerializer.Serialize(leash));
+                }
+
+                Console.WriteLine("Press 1 to add a product.");
+                Console.WriteLine("Type 'exit' to quit.");
+                userInput = Console.ReadLine();
+            }
+          
         }
     }
 }

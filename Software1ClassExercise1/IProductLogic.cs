e6e12bf838d54ee9d1903c5c17a3e7bdbf1063e2
Brent Reynolds <brentsreynolds5@gmail.com>
2024-05-03 16:25:07 -0400

    Created ProductLogic interface

---
+++ Software1ClassExercise1/IProductLogic.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Software1ClassExercise1
+{
+    public interface IProductLogic
+    {
+        void AddProduct(Product product);
+        List<Product> GetAllProducts();
+        CatFood GetCatFoodByName(string name);
+        DogLeash GetDogLeashByName(string name);
+        DryCatFood GetDryCatFoodByName(string name);
+        List<string> GetOnlyInStockProducts();
+    }
+}

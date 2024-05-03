a14d03f4082c43dfda31cdcaa23a6d70cf390973
Brent Reynolds <brentsreynolds5@gmail.com>
2024-05-03 16:24:15 -0400

    Implemented Product Logic interface

--- Software1ClassExercise1/ProductLogic.cs
+++ Software1ClassExercise1/ProductLogic.cs
@@ -3,25 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Software1ClassExercise1;
 
 namespace Software1ClassExercise1
 {
-    public class ProductLogic
+    public class ProductLogic : IProductLogic
     {
         private List<Product> _products = new List<Product>();
         private Dictionary<string, DogLeash> _dogLeashes = new Dictionary<string, DogLeash>();
         private Dictionary<string, CatFood> _catFoods = new Dictionary<string, CatFood>();
         private Dictionary<string, DryCatFood> _dryCatFoods = new Dictionary<string, DryCatFood>();
 
-        public ProductLogic() { 
-            _products = new List<Product>();
+        public ProductLogic()
+        {
+            _products = new List<Product>
+
+            {
+                new DogLeash
+                {
+                    Description = "A rope dog leash made from strong rope.",
+                    LengthInches = 60,
+                    Material = "Rope",
+                    Name = "Rope Dog Leash",
+                    Price = 21.00m,
+                    Quantity = 0
+                },
+                new DryCatFood
+                {
+                    Quantity = 6,
+                    Price = 25.59m,
+                    Name = "Plain 'Ol Cat Food",
+                    Description = "Nothing fancy to find here.  Just the basic stuff your cat needs to live a healthy life",
+                    WeightPounds = 10,
+                    KittenFood = false
+                },
+                new CatFood
+                {
+                    Quantity = 48,
+                    Price = 12.99m,
+                    Name = "Fancy Cat Food",
+                    Description = "Food that isn't only delicious, but made from the finest of all cat food stuff",
+                    KittenFood = false
+                }
+            };
         }
+
+        public List<string> GetOnlyInStockProducts()
+        {
+            return _products.Where(x => x.Quantity > 0).Select(x => x.Name).ToList();
+        }
+
 
         public void AddProduct(Product product)
         {
             _products.Add(product);
 
-            if(product is DogLeash)
+            if (product is DogLeash)
             {
                 DogLeash leash = (DogLeash)product;
                 _dogLeashes.Add(leash.Name, leash);
@@ -39,16 +76,17 @@ namespace Software1ClassExercise1
                 _catFoods.Add(food.Name, food);
                 Console.WriteLine("Cat food added to inventory.");
             }
-            
+
         }
 
-        public List<Product> GetAllProducts() {
+        public List<Product> GetAllProducts()
+        {
             return _products;
         }
 
         public DogLeash GetDogLeashByName(string name)
         {
-            
+
             if (_dogLeashes.ContainsKey(name))
             {
                 return _dogLeashes[name];
@@ -58,7 +96,7 @@ namespace Software1ClassExercise1
                 Console.WriteLine("The leash name does not exist in the inventory.");
                 return null;
             }
-            
+
         }
 
         public CatFood GetCatFoodByName(string name)
@@ -73,7 +111,7 @@ namespace Software1ClassExercise1
                 return null;
             }
         }
-        
+
         public DryCatFood GetDryCatFoodByName(string name)
         {
             if (_dryCatFoods.ContainsKey(name))
@@ -87,4 +125,5 @@ namespace Software1ClassExercise1
             }
         }
     }
+
 }
